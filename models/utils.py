fa56d6d10a8ce585af39f02d9fc27aecd266f4d3
Kenenbek R <kenenbek@gmail.com>
2021-06-25 01:31:08 +0300

    add eval step

--- models/utils.py
+++ models/utils.py
@@ -1,5 +1,6 @@
 import gin
 import trax
+import trax.layers as tl
 from .data_gen_np import get_generator
 from trax.supervised import training
 
@@ -21,18 +22,24 @@ def get_trax_generator(num_genomes, genome_length, num_generations,
 def train(model, train_gen, comet_exp, lr, n_warmup_steps, n_steps_per_checkpoint, output_dir, n_steps):
     lr_schedule = trax.lr.warmup_and_rsqrt_decay(
         n_warmup_steps=n_warmup_steps, max_value=lr)
-    
     train_task = training.TrainTask(
         labeled_data=train_gen,
-        loss_layer=trax.layers.CategoryCrossEntropy(),
+        loss_layer=tl.CategoryCrossEntropy(),
         optimizer=trax.optimizers.Adam(lr),
         lr_schedule=lr_schedule,
         n_steps_per_checkpoint=n_steps_per_checkpoint
     )
     
-    loop = training.Loop(model,
-                         train_task,
-                         output_dir=output_dir)
+    eval_task = training.EvalTask(
+        labeled_data=train_gen,
+        metrics=[tl.CategoryCrossEntropy()]
+    )
+    
+    loop = training.Loop(model=model,
+                         tasks=train_task,
+                         eval_tasks=eval_task,
+                         output_dir=output_dir,
+                         eval_at=lambda x: x % 10 == 0)
     
     with comet_exp.train():
         loop.run(n_steps=n_steps)
