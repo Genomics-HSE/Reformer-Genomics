23dda86ec92a31d5e8bf435676b301fda3c54034
Kenenbek R <kenenbek@gmail.com>
2021-06-25 01:38:58 +0300

    add offline comet ml

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ if __name__ == '__main__':
     gin.parse_config_file("config.gin")
     
     train_gen = get_trax_generator()
-    comet_exp = comet_ml.Experiment()
+    comet_exp = comet_ml.OfflineExperiment(project_name="population-genomics-new",
+                                           workspace="kenenbek")
     
     train(model=ReformerModel(), train_gen=train_gen, comet_exp=comet_exp)
