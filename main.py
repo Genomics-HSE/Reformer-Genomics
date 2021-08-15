7cfdfb9047f03683f9fc452ba3101a0f1446b773
Kenenbek R <kenenbek@gmail.com>
2021-08-15 13:35:21 +0300

    predict model fix

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import comet_ml
 import gin
 import argparse
-from models import train_model, predict
-
+from models import train_model, predict_model
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog='Genomics')
@@ -13,7 +12,7 @@ if __name__ == '__main__':
     predict_parser = action_parsers.add_parser('predict')
     predict_parser.add_argument('--path', type=str)
     args = parser.parse_args()
-
+    
     gin.parse_config_file(args.config)
     
     if args.action == "train":
@@ -21,9 +20,9 @@ if __name__ == '__main__':
                     lr=gin.REQUIRED, n_warmup_steps=gin.REQUIRED, n_steps_per_checkpoint=gin.REQUIRED,
                     output_dir=gin.REQUIRED, n_steps=gin.REQUIRED)
     elif args.action == "predict":
-        predict(model_path=args.path, model=gin.REQUIRED,
-                data_gen=gin.REQUIRED, num_genomes=gin.REQUIRED,
-                plot_dir=gin.REQUIRED, plot_length=gin.REQUIRED
-                )
+        predict_model(model_path=args.path, model=gin.REQUIRED,
+                      data_gen=gin.REQUIRED, num_genomes=gin.REQUIRED,
+                      plot_dir=gin.REQUIRED, plot_length=gin.REQUIRED
+                      )
     else:
         ValueError("Choose train or predict")
